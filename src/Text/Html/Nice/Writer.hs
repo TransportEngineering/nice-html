f797c4806f60905be15de90488c013ed381e607d
Mike Ledger <eleventynine@gmail.com>
2017-06-03 05:42:37 +1000

    cool new combinator for react-esque server side rendering

--- src/Text/Html/Nice/Writer.hs
+++ src/Text/Html/Nice/Writer.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE BangPatterns           #-}
 {-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE DeriveFoldable         #-}
+{-# LANGUAGE DeriveFunctor          #-}
+{-# LANGUAGE DeriveTraversable      #-}
 {-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE GADTs                  #-}
 {-# LANGUAGE MultiParamTypeClasses  #-}
@@ -14,53 +17,80 @@ module Text.Html.Nice.Writer where
 import           Data.Foldable          (toList)
 import           Data.String            (IsString (..))
 import           Data.Text              (Text)
+import qualified Data.Text              as T
 import qualified Data.Text.Lazy         as TL
 import qualified Data.Text.Lazy.Builder as TLB
 import qualified Data.Vector            as V
 import           Text.Html.Nice
 
-newtype Markup p a = Markup ([Attr p] -> ([Markup' p] -> [Markup' p], a))
+
+type Children p = [Markup' p] -> [Markup' p]
+
+data MarkupStep p a = MarkupStep
+  { msGlobalId :: {-# UNPACK #-} !Int
+  , msChildren :: Children p
+  , msResult   :: a
+  } deriving (Functor, Foldable, Traversable)
+
+newtype Markup p a = Markup (Int -> [Attr p] -> MarkupStep p a)
 
 instance Functor (Markup p) where
-  fmap f (Markup m) = Markup (\attr -> fmap f (m attr))
+  fmap f (Markup m) = Markup (\i attr -> fmap f (m i attr))
 
 instance Applicative (Markup p) where
-  pure a = Markup (\_ -> (id, a))
-  Markup f <*> Markup x = Markup $ \attr ->
-    case f attr of
-      (fnodes, f') -> case x [] of
-        (xnodes, x') -> (fnodes . xnodes, f' x')
+  pure a = Markup (\i _ -> MarkupStep i id a)
+  Markup f <*> Markup x = Markup $ \i attr ->
+    case f i attr of
+      MarkupStep j fnodes f' ->
+        case x j [] of
+          MarkupStep k xnodes x' ->
+            MarkupStep k (fnodes . xnodes) (f' x')
 
 instance Monad (Markup p) where
-  Markup mx >>= f = Markup $ \attr -> case mx attr of
-    (dx, a) -> case f a of
-      Markup a' -> case a' [] of
-        (dy, b) -> (dx . dy, b)
+  Markup mx >>= f = Markup $ \i attr -> case mx i attr of
+    MarkupStep j dx a -> case f a of
+      Markup f' -> case f' j attr of
+        MarkupStep k dy b -> MarkupStep k (dx . dy) b
 
 -- | Compile a 'Markup'. Don't run this multiple times!
 compile :: Markup t a -> FastMarkup t
-compile m = case runM m of (m', _) -> compile_ m'
+compile m = case runM 0 m of (m', _, _) -> compile_ m'
+
+-- | Compile a 'Markup'. Don't run this multiple times!
+compileE :: Markup t a -> (a, FastMarkup t)
+compileE m = case runM 0 m of (m', _, a) -> (a, compile_ m')
 
 --------------------------------------------------------------------------------
 -- Internals
 
-runM :: Markup t a -> (Markup' t, a)
-runM (Markup m) = (List (x []), a) where (x, a) = m []
+runM :: Int -> Markup t a -> (Markup' t, Int, a)
+runM i (Markup m) = (List (x []), j, a) where MarkupStep j x a = m i []
 
 {-# INLINE makeElement #-}
 makeElement :: Text -> Markup p a -> Markup p a
 makeElement name m =
-  Markup $ \attr -> case runM m of
-    (cs, a) -> ((:) (Node name (V.fromList attr) cs), a)
+  Markup $ \i attr -> case runM i m of
+    (cs, j, a) -> MarkupStep
+      { msGlobalId = j
+      , msChildren = (:) (Node name (V.fromList attr) cs)
+      , msResult = a
+      }
 
 {-# INLINE makeVoidElement #-}
 makeVoidElement :: Text -> Markup p ()
-makeVoidElement name =
-  Markup $ \attr -> ((:) (VoidNode name (V.fromList attr)), ())
+makeVoidElement name = Markup $ \i attr -> MarkupStep
+  { msGlobalId = i
+  , msChildren = (:) (VoidNode name (V.fromList attr))
+  , msResult = ()
+  }
 
 {-# INLINE lift #-}
 lift :: Markup' t -> Markup t ()
-lift m' = Markup (\_ -> ((m':), ()))
+lift m' = Markup $ \i _ -> MarkupStep
+  { msGlobalId = i
+  , msChildren = (m':)
+  , msResult = ()
+  }
 
 --------------------------------------------------------------------------------
 -- Node types
@@ -75,14 +105,21 @@ instance a ~ () => IsString (Markup t a) where
 --------------------------------------------------------------------------------
 -- Combinators
 
-type Node t a = Markup t a -> Markup t a
-
 {-# INLINE (!) #-}
 -- | Add some attributes
-(!) :: Node t a -> [Attr t] -> Markup t a -> Markup t a
-(!) f a x = Markup $ \attrs ->
+(!) :: (Markup t a -> Markup t b) -> [Attr t] -> Markup t a -> Markup t b
+(!) f a x = Markup $ \i attrs ->
   case f x of
-    Markup m -> m (a ++ attrs)
+    Markup m -> m i (a ++ attrs)
+
+-- | Give a node a unique id
+--
+-- Might be handy to build server-side react-esque systems
+note :: (Markup t a -> Markup t b) -> Markup t a -> Markup t Int
+note f x = Markup $ \i attrs ->
+  case f x of
+    Markup m ->
+      (m (i + 1) ("id" := T.pack (show i):attrs)) { msResult = i }
 
 infixl 8 !
 
