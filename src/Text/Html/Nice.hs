4cc6d689b5afe6509d97edc37b14343995527793
Mike Ledger <mike@quasimal.com>
2017-11-16 15:13:38 +1100

    don't re-export lots of stuff. it's annoying

--- src/Text/Html/Nice.hs
+++ src/Text/Html/Nice.hs
@@ -15,22 +15,9 @@ module Text.Html.Nice
     -- * Utility
   , recompile
   , unlayer
-    -- * Re-exports of 'TLB.Builder' functions
-  , module Data.Text.Lazy.Builder
-  , module Data.Text.Lazy.Builder.Int
-  , module Data.Text.Lazy.Builder.RealFloat
-    -- * Re-exports of lazy 'Text' functions
-  , Text
-  , fromStrict
-  , toStrict
   ) where
-import           Data.Text.Lazy                   (Text, fromStrict, toStrict)
-import           Data.Text.Lazy.Builder
-import           Data.Text.Lazy.Builder.Int
-import           Data.Text.Lazy.Builder.RealFloat
-import           Text.Html.Nice.Internal          ((:$) (..), Attr (..),
-                                                   FastMarkup, Render (..),
-                                                   recompile, render, renderM,
-                                                   renderMs, unlayer)
+import           Text.Html.Nice.Internal     ((:$) (..), Attr (..), FastMarkup,
+                                              Render (..), recompile, render,
+                                              renderM, renderMs, unlayer)
 import           Text.Html.Nice.Writer
 import           Text.Html.Nice.Writer.Html5
